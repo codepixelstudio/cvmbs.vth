a41ed29a4abd80b4eb71a35cc15c5da99f88b386
codepixelstudio <codepixel@codepixelstudio.com>
2021-01-20 11:36:58 -0700

    [SERVICE] render styled header

--- template-service-child.php
+++ template-service-child.php
@@ -20,11 +20,7 @@
 		<!-- service post -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-            <h1>
-
-                <?php the_title(); ?>
-
-            </h1>
+			<?php get_template_part( $block_path . 'page-header' ); ?>
 
 			<div class="flexible-page-content">
 
