71097cd9435f3c590e6cd99873366c267bd216aa
codepixelstudio <codepixel@codepixelstudio.com>
2021-06-10 23:45:22 -0600

    [CLINICAL TRIAL] reorder filter controls and trial list in filtered view

--- archive-clinical_trial.php
+++ archive-clinical_trial.php
@@ -187,6 +187,143 @@
 
         <?php endif; ?>
 
+        <?php if ( $query_URL ) : ?>
+
+        <!-- grid -->
+        <div id="news_grid" class="fixed_width experimental filtered_view">
+
+            <?php if ( $clinical_trials_query->have_posts() ) : ?>
+
+        	<?php while ( $clinical_trials_query->have_posts() ) : $clinical_trials_query->the_post(); ?>
+
+            <?php
+
+                // post vars
+                $topic    = get_the_category();
+                $author   = get_the_author();
+                $publish  = get_the_date();
+                $image    = has_post_thumbnail() ? 'style="background-image:url(' . get_the_post_thumbnail_url( get_the_id(), 'x-large' ) . ');"' : '';
+
+                // get redirect status
+                $redirect = get_field( 'post_redirect' );
+
+                // set link var
+                $permalink;
+
+                // test for redirect
+                if ( $redirect ) {
+
+                    $permalink = get_field( 'redirect_url' );
+
+                } else {
+
+                    $permalink = get_the_permalink();
+
+                }
+
+            ?>
+
+            <!-- post -->
+            <a href="<?php echo $permalink; ?>" <?php post_class(); ?>>
+
+                <?php the_title(); ?>
+
+            </a>
+            <!-- END post -->
+
+        	<?php endwhile; ?>
+
+            <?php else : ?>
+
+            Currently, there are no active/ongoing clinical trials matching the selected criteria.
+
+            <?php endif; ?>
+
+        </div>
+        <!-- END grid -->
+
+        <!-- news navigation -->
+        <div id="news_controls" class="news_navigation fixed_width <?php echo $dynamic_layout; ?>">
+
+            <!-- navigation -->
+            <aside id="news_taxonomy">
+
+                <!-- metadata group -->
+                <section class="metadata tags">
+
+                    <!-- title -->
+                    <span class="metadata_title">
+
+                        browse clinical trials
+
+                    </span>
+                    <!-- END title -->
+
+                    <!-- taxonomy group -->
+                    <div class="taxonomy_group">
+
+                        <?php
+
+                            // setup tag cloud
+                            $tags = get_terms( array(
+
+                                'taxonomy'   => 'clinical_trial_tag',
+                                'hide_empty' => true,
+
+                            ));
+
+                            // tag cloud iteration
+                            foreach ( $tags as $tag ) {
+
+                                $tag_link = get_tag_link( $tag->term_id );
+
+                                // retrieve query string
+                                $query_URL = $_GET[ 'tag' ];
+
+                                if ( $tag->slug == $query_URL ) {
+
+                                    $active_class = 'active';
+
+                                } else {
+
+                                    $active_class = 'inactive';
+
+                                }
+
+                                $tag_list .= '
+
+                                    <a href="?tag=' . $tag->slug . '" class="taxonomy_item ' . $active_class . '">
+
+                                        ' . $tag->name . '
+
+                                    </a>
+
+                                ';
+
+                            }
+
+                            // output tags
+                            echo $tag_list;
+
+                            // view all
+                            echo '<a href="' . get_site_url() . '/clinical-trials" class="taxonomy_item clear">all</a>';
+
+                        ?>
+
+                    </div>
+                    <!-- END taxonomy group -->
+
+                </section>
+                <!-- END metadata group -->
+
+            </aside>
+            <!-- END navigation -->
+
+        </div>
+        <!-- END news navigation -->
+
+        <?php else : ?>
+
         <!-- news navigation -->
         <div id="news_controls" class="news_navigation fixed_width <?php echo $dynamic_layout; ?>">
 
@@ -319,6 +456,8 @@
 
         </div>
         <!-- END grid -->
+
+        <?php endif; ?>
 
         <?php if ( !$query_URL ) : ?>
 
